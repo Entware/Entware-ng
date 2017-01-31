c97be5883d958dea68613a3adebbfe7946e494b2
Alexander Ryzhov <github@ryzhov-al.ru>
2017-01-31 21:33:05 +0300

    scripts: LEDE sync, 2017.01

--- scripts/rstrip.sh
+++ scripts/rstrip.sh
@@ -30,6 +30,16 @@ find $TARGETS -type f -a -exec file {} \; | \
 		eval "$STRIP_KMOD $F"
 	} || {
 		b=$(stat -c '%a' $F)
+		[ -z "$PATCHELF" ] || [ -z "$TOPDIR" ] || {
+			old_rpath="$($PATCHELF --print-rpath $F)"; new_rpath=""
+			for path in $old_rpath; do
+				case "$path" in
+					/lib/[^/]*|/usr/lib/[^/]*|\$ORIGIN/*) new_rpath="${new_rpath:+$new_rpath:}$path" ;;
+					*) echo "$SELF: $F: removing rpath $path" ;;
+				esac
+			done
+			[ "$new_rpath" = "$old_rpath" ] || $PATCHELF --set-rpath "$new_rpath" $F
+		}
 		eval "$STRIP $F"
 		a=$(stat -c '%a' $F)
 		[ "$a" = "$b" ] || chmod $b $F

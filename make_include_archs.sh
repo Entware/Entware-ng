3e4f55e58467c8cdcd4ab14245b20fe31cf4ab6b
zyxmon <zyxmon@gmail.com>
2017-02-18 09:05:14 +0300

    fix path in make_.. script

--- make_include_archs.sh
+++ make_include_archs.sh
@@ -2,5 +2,5 @@
 
 tar czvf bin/targets/armv7soft/generic-glibc/include.tar.gz -C ./staging_dir/target-arm_cortex-a9_glibc-2.23_eabi/opt/include/ .
 tar czvf bin/targets/armv5soft/generic-glibc/include.tar.gz -C ./staging_dir/target-arm_xscale_glibc-2.23_eabi/opt/include/ .
-tar czvf bin/targets/x86-32/generic-glibc/include.tar.gz    -C ./staging_dir/target-i386_i486_glibc-2.23/opt/include/ .
+tar czvf bin/targets/x86-32/generic-glibc/include.tar.gz    -C ./staging_dir/target-i386_pentium_glibc-2.23/opt/include/ .
 tar czvf bin/targets/x86-64/generic-glibc/include.tar.gz    -C ./staging_dir/target-x86_64_glibc-2.23/opt/include/ .
